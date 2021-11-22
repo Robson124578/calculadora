584c6ed6b0f5c395857012aefe29c6de5b531a4e
Robson124578 <robim.net@gmail.com>
2021-11-21 23:39:21 -0300

    layout foi estruturado. Começando a lógica da aplicação

---
+++ js/action.js
@@ -0,0 +1,28 @@
+// Definições dos bottões
+
+var tcl1 = document.querySelector(".tcl-1");
+var tcl2 = document.querySelector(".tcl-2");
+var tcl3 = document.querySelector(".tcl-3");
+var tcl4 = document.querySelector(".tcl-4");
+var tcl5 = document.querySelector(".tcl-5");
+var tcl6 = document.querySelector(".tcl-6");
+var tcl7 = document.querySelector(".tcl-7");
+var tcl8 = document.querySelector(".tcl-8");
+var tcl9 = document.querySelector(".tcl-9");
+var tcl0 = document.querySelector(".tcl-0");
+var tclDivs = document.querySelector(".tcl-divs");
+var tclMult = document.querySelector(".tcl-mult");
+var tclSubt = document.querySelector(".tcl-subt");
+var tclAdic = document.querySelector(".tcl-adic");
+
+
+var display = document.querySelector(".display-calculadora");
+
+tcl1.addEventListener("click", function() {
+
+    var num = tcl1.innerText
+
+    alert(num);
+
+
+});
