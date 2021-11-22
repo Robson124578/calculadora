6acf776bfc9d93675a689eb532d9c17036f7ba65
Robson124578 <robim.net@gmail.com>
2021-11-22 16:05:47 -0300

    alterações no layout e modelando a lógica

--- js/action.js
+++ js/action.js
@@ -1,28 +1,37 @@
 // Definições dos bottões
 
-var tcl1 = document.querySelector(".tcl-1");
-var tcl2 = document.querySelector(".tcl-2");
-var tcl3 = document.querySelector(".tcl-3");
-var tcl4 = document.querySelector(".tcl-4");
-var tcl5 = document.querySelector(".tcl-5");
-var tcl6 = document.querySelector(".tcl-6");
-var tcl7 = document.querySelector(".tcl-7");
-var tcl8 = document.querySelector(".tcl-8");
-var tcl9 = document.querySelector(".tcl-9");
-var tcl0 = document.querySelector(".tcl-0");
-var tclDivs = document.querySelector(".tcl-divs");
-var tclMult = document.querySelector(".tcl-mult");
-var tclSubt = document.querySelector(".tcl-subt");
-var tclAdic = document.querySelector(".tcl-adic");
+var display = document.querySelector(".texto_display");
 
 
-var display = document.querySelector(".display-calculadora");
+loteNumeros1 = []
 
-tcl1.addEventListener("click", function() {
+function calcular() {
 
-    var num = tcl1.innerText
+    // capturando o evento de clique
+    var evento = window.event;
+    var num = evento.target;
+    var text = num.innerText;
 
-    alert(num);
+    loteNumeros1.push(text); // adicionando os elementos no array
+
+    if (text == "Del") {
+        loteNumeros1.pop();
+        text = document.createTextNode(loteNumeros1)
+        display.appendChild(text)
+    }
+
+    if (text == "×") {
+        alert('clicou no x')
+    }
+
+    var numrosJuntos = loteNumeros1.join(""); //unindo os elementos do array
+
+    var numeroComoInt = parseInt(numrosJuntos); //convertendo para inteiro
 
 
-});
+    // inserindo valores visíveis no na tela
+    text = document.createTextNode(text)
+    display.appendChild(text)
+
+
+}
